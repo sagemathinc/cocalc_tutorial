dda83c32d38032f0260cfbc934831b96737151e3
Tania Allard <t.allard@sheffield.ac.uk>
2017-03-14 11:46:46 +0000

    Fixed heading formatting
    Also, fixed some typos on the student's section

---
+++ scripts/current.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('a').each(function(){
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('active'); $(this).parents('li').addClass('active');
+        }
+    });
+});
